236c5eb49c1cfe50ca43fd2aefa0c2fe540433d1
ekaspersen <89420442+ekaspersen@users.noreply.github.com>
2023-05-04 15:18:55 +0200

    while website is being buildt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import Footer from "./sections/Footer";
-import HeroBanner from "./sections/HeroBanner";
 import Services from "./sections/Services";
-import Welcome from "./sections/Welcome";
 
 function App() {
     return (
         <>
-            <HeroBanner />
-            <Welcome />
             <Services />
             <Footer />
         </>

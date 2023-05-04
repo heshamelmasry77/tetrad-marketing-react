5b5f6974e44f99bdc8b94213412681d8ff29d72a
ekaspersen <89420442+ekaspersen@users.noreply.github.com>
2023-05-04 15:21:44 +0200

    qhile website is being buildt

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -7,13 +7,8 @@ function Services() {
     return (
         <div id="tjenester" className="bg-offblack mb-16">
             <div className="flex flex-col gap-16 py-32">
-                <h2 className=" mx-auto text-5xl font-bold w-fit border-b-pink border-b-4 pb-2 ">
-                    Komplett Digital Markedsføring
-                </h2>
                 <div className="inner flex justify-between">
                     <ServiceDigsyn />
-                    <ServiceInnhold />
-                    <ServiceMarkeds />
                 </div>
             </div>
         </div>
